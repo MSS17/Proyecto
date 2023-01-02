d79d2b8dde8fd1af058697fcb4580117e0e2dfc5
MSS17 <miguelsandoval20470266@gmail.com>
2023-01-01 23:30:34 -0600

    fimal

---
+++ app.js
@@ -0,0 +1,604 @@
+const listaDeImagenes = [    	
+    {
+    "id":102,
+    "download_url":	"https://picsum.photos/id/102/4320/3240"
+    },
+    {
+    "id":103,
+    "download_url":	"https://picsum.photos/id/103/2592/1936"
+    },
+    {
+    "id":104,
+    "download_url":	"https://picsum.photos/id/104/3840/2160"
+    },
+    {
+    "id":105,
+    "download_url":	"https://picsum.photos/id/5/5000/3334"
+    },
+    {
+    "id":106,
+    "download_url":	"https://picsum.photos/id/106/2592/1728"
+    },
+    {
+    "id":107,
+    "download_url":	"https://picsum.photos/id/107/5000/3333"
+    },
+    {
+    "id":108,
+    "download_url":	"https://picsum.photos/id/108/2000/1333"
+    },
+    {
+    "id":109,
+    "download_url":	"https://picsum.photos/id/109/4287/2392"
+    },
+    {
+    "id":110,
+    "download_url":	"https://picsum.photos/id/110/5000/3333"
+    },
+    {
+    "id":111,
+    "download_url":	"https://picsum.photos/id/111/4400/2656"
+    },
+    {
+    "id":112,
+    "download_url":	"https://picsum.photos/id/112/4200/2800"
+    },
+    {
+    "id":113,
+    "download_url":	"https://picsum.photos/id/113/4168/2464"
+    },
+    {
+    "id":114,
+    "download_url":	"https://picsum.photos/id/114/3264/2448"
+    },
+    {
+    "id":115,
+    "download_url":	"https://picsum.photos/id/115/1500/1000"
+    },
+    {
+    "id":116,
+    "download_url":	"https://picsum.photos/id/116/3504/2336"
+    },
+    {
+    "id":117,
+    "download_url":	"https://picsum.photos/id/117/1544/1024"
+    },
+    {
+    "id":118,
+    "download_url":	"https://picsum.photos/id/118/1500/1000"
+    },
+    {
+    "id":119,
+    "download_url":	"https://picsum.photos/id/119/3264/2176"
+    },
+    {
+    "id":120,
+    "download_url":	"https://picsum.photos/id/120/4928/3264"
+    },
+    {
+    "id":121,
+    "download_url":	"https://picsum.photos/id/121/1600/1067"
+    },
+    {
+    "id":122,
+    "download_url":	"https://picsum.photos/id/122/4147/2756"
+    },
+    {
+    "id":123,
+    "download_url":	"https://picsum.photos/id/123/4928/3264"
+    },
+    {
+    "id":124,
+    "download_url":	"https://picsum.photos/id/124/3504/2336"
+    },
+    {
+    "id":125,
+    "download_url":	"https://picsum.photos/id/125/1500/1000"
+    },
+    {
+    "id":126,
+    "download_url":	"https://picsum.photos/id/126/4272/2511"
+    },
+    {
+    "id":127,
+    "download_url":	"https://picsum.photos/id/127/4032/2272"
+    },
+    {
+    "id":128,
+    "download_url":	"https://picsum.photos/id/128/3823/2549"
+    },
+    {
+    "id":129,
+    "download_url":	"https://picsum.photos/id/129/4910/3252"
+    },
+    {
+    "id":130,
+    "download_url":	"https://picsum.photos/id/130/3807/2538"
+    },
+    {
+    "id":131,
+    "download_url":	"https://picsum.photos/id/131/4698/3166"
+    },
+    {
+    "id":132,
+    "download_url":	"https://picsum.photos/id/132/1600/1066"
+    },
+    {
+    "id":133,
+    "download_url":	"https://picsum.photos/id/133/2742/1828"
+    },
+    {
+    "id":134,
+    "download_url":	"https://picsum.photos/id/134/4928/3264"
+    },
+    {
+    "id":135,
+    "download_url":	"https://picsum.photos/id/135/2560/1920"
+    },
+    {
+    "id":136,
+    "download_url":	"https://picsum.photos/id/136/4032/2272"
+    },
+    {
+    "id":137,
+    "download_url":	"https://picsum.photos/id/137/4752/3168"
+    },
+    {
+    "id":138,
+    "download_url":	"https://picsum.photos/id/8/5000/3333"
+    },
+    {
+    "id":139,
+    "download_url":	"https://picsum.photos/id/139/3465/3008"
+    },
+    {
+    "id":140,
+    "download_url":	"https://picsum.photos/id/140/2448/2448"
+    },
+    {
+    "id":141,
+    "download_url":	"https://picsum.photos/id/141/2048/1365"
+    },
+    {
+    "id":142,
+    "download_url":	"https://picsum.photos/id/142/4272/2848"
+    },
+    {
+    "id":143,
+    "download_url":	"https://picsum.photos/id/143/3600/2385"
+    },
+    {
+    "id":144,
+    "download_url":	"https://picsum.photos/id/144/4912/2760"
+    },
+    {
+    "id":145,
+    "download_url":	"https://picsum.photos/id/145/4288/2848"
+    },
+    {
+    "id":146,
+    "download_url":	"https://picsum.photos/id/146/5000/3333"
+    },
+    {
+    "id":147,
+    "download_url":	"https://picsum.photos/id/147/2448/2448"
+    },
+    {
+    "id":148,
+    "download_url":	"https://picsum.photos/id/18/2500/1667"
+    },
+    {
+    "id":149,
+    "download_url":	"https://picsum.photos/id/149/3454/2288"
+    },
+    {
+    "id":150,
+    "download_url":	"https://picsum.photos/id/10/2500/1667"
+    },
+    {
+    "id":151,
+    "download_url":	"https://picsum.photos/id/151/4288/3216"
+    },
+    {
+    "id":152,
+    "download_url":	"https://picsum.photos/id/152/3888/2592"
+    },
+    {
+    "id":153,
+    "download_url":	"https://picsum.photos/id/153/4763/3155"
+    },
+    {
+    "id":154,
+    "download_url":	"https://picsum.photos/id/154/3264/2176"
+    },
+    {
+    "id":155,
+    "download_url":	"https://picsum.photos/id/155/3264/2176"
+    },
+    {
+    "id":156,
+    "download_url":	"https://picsum.photos/id/156/2177/3264"
+    },
+    {
+    "id":157,
+    "download_url":	"https://picsum.photos/id/157/5000/3914"
+    },
+    {
+    "id":158,
+    "download_url":	"https://picsum.photos/id/158/4836/3224"
+    },
+    {
+    "id":159,
+    "download_url":	"https://picsum.photos/id/159/5000/2460"
+    },
+    {
+    "id":160,
+    "download_url":	"https://picsum.photos/id/160/3200/2119"
+    },
+    {
+    "id":161,
+    "download_url":	"https://picsum.photos/id/161/4240/2832"
+    },
+    {
+    "id":162,
+    "download_url":	"https://picsum.photos/id/162/1500/998"
+    },
+    {
+    "id":163,
+    "download_url":	"https://picsum.photos/id/163/2000/1333"
+    },
+    {
+    "id":164,
+    "download_url":	"https://picsum.photos/id/164/1200/800"
+    },
+    {
+    "id":165,
+    "download_url":	"https://picsum.photos/id/165/2000/1333"
+    },
+    {
+    "id":166,
+    "download_url":	"https://picsum.photos/id/166/1280/720"
+    },
+    {
+    "id":167,
+    "download_url":	"https://picsum.photos/id/167/2896/1944"
+    },
+    {
+    "id":168,
+    "download_url":	"https://picsum.photos/id/168/1920/1280"
+    },
+    {
+    "id":169,
+    "download_url":	"https://picsum.photos/id/169/2500/1662"
+    },
+    {
+    "id":170,
+    "download_url":	"https://picsum.photos/id/170/2500/1667"
+    },
+    {
+    "id":171,
+    "download_url":	"https://picsum.photos/id/171/2048/1536"
+    },
+    {
+    "id":172,
+    "download_url":	"https://picsum.photos/id/172/2000/1325"
+    },
+    {
+    "id":173,
+    "download_url":	"https://picsum.photos/id/173/1200/737"
+    },
+    {
+    "id":174,
+    "download_url":	"https://picsum.photos/id/174/1600/589"
+    },
+    {
+    "id":175,
+    "download_url":	"https://picsum.photos/id/175/2896/1944"
+    },
+    {
+    "id":176,
+    "download_url":	"https://picsum.photos/id/176/2500/1662"
+    },
+    {
+    "id":177,
+    "download_url":	"https://picsum.photos/id/177/2515/1830"
+    },
+    {
+    "id":178,
+    "download_url":	"https://picsum.photos/id/178/2592/1936"
+    },
+    {
+    "id":179,
+    "download_url":	"https://picsum.photos/id/179/2048/1365"
+    },
+    {
+    "id":180,
+    "download_url":	"https://picsum.photos/id/180/2400/1600"
+    },
+    {
+    "id":181,
+    "download_url":	"https://picsum.photos/id/181/1920/1189"
+    },
+    {
+    "id":182,
+    "download_url":	"https://picsum.photos/id/182/2896/1944"
+    },
+    {
+    "id":183,
+    "download_url":	"https://picsum.photos/id/183/2316/1544"
+    },
+    {
+    "id":184,
+    "download_url":	"https://picsum.photos/id/184/4288/2848"
+    },
+    {
+    "id":185,
+    "download_url":	"https://picsum.photos/id/185/3995/2662"
+    },
+    {
+    "id":186,
+    "download_url":	"https://picsum.photos/id/186/2048/1275"
+    },
+    {
+    "id":187,
+    "download_url":	"https://picsum.photos/id/187/4000/2667"
+    },
+    {
+    "id":188,
+    "download_url":	"https://picsum.photos/id/188/2896/1936"
+    },
+    {
+    "id":189,
+    "download_url":	"https://picsum.photos/id/189/2048/1536"
+    },
+    {
+    "id":190,
+    "download_url":	"https://picsum.photos/id/190/2048/1365"
+    },
+    {
+    "id":191,
+    "download_url":	"https://picsum.photos/id/191/2560/1707"
+    },
+    {
+    "id":192,
+    "download_url":	"https://picsum.photos/id/192/2352/2352"
+    },
+    {
+    "id":193,
+    "download_url":	"https://picsum.photos/id/193/3578/2451"
+    },
+    {
+    "id":194,
+    "download_url":	"https://picsum.photos/id/194/2000/1325"
+    },
+    {
+    "id":195,
+    "download_url":	"https://picsum.photos/id/195/768/1024"
+    },
+    {
+    "id":196,
+    "download_url":	"https://picsum.photos/id/196/2048/1536"
+    },
+    {
+    "id":197,
+    "download_url":	"https://picsum.photos/id/197/4272/2848"
+    },
+    {
+    "id":198,
+    "download_url":	"https://picsum.photos/id/198/3456/2304"
+    },
+    {
+    "id":199,
+    "download_url":	"https://picsum.photos/id/199/2592/1728"
+    },
+    {
+    "id":200,
+    "download_url":	"https://picsum.photos/id/200/1920/1280"
+    },
+    {
+    "id":201,
+    "download_url":	"https://picsum.photos/id/201/5000/3333"
+    },
+    {
+    "id":202,
+    "download_url":	"https://picsum.photos/id/202/2392/1260"
+    },
+    {
+    "id":203,
+    "download_url":	"https://picsum.photos/id/203/4032/3024"
+    },
+    {
+    "id":204,
+    "download_url":	"https://picsum.photos/id/204/5000/3333"
+    }
+];
+document.body.innerHTML = `
+    <div class="contenedorgeneral">
+        <div class="pc">
+            <header>
+                <a href="#" class="logo"><img src="logo_bz.webp" alt=""></a>
+                <div class="barramenu">
+                    <ul class="menu">
+                        <li ><a href="#">Inicio</a></li>
+                        <li ><a href="#">Servicios</a></li>
+                        <li ><a href="#">Apps</a></li>
+                        <li ><a href="#">Trabajos</a></li>
+                        <li ><a href="#">Ofertas</a></li>
+                        <li ><a href="#">Demo</a></li>
+                        <li ><a href="#" class="Ejemplo">Ejemplo</a>
+                        <li ><a href="#">Blog</a></li>
+                        <li ><a href="#">Contacto</a></li>
+                        <ion-icon class="btncerrar" name="close-outline"></ion-icon>
+                    </ul>
+                    </div> 
+                    <ion-icon class="btnmenu" name="menu-outline"></ion-icon>
+                </div>
+            </header>
+            <section>
+                <div class="imagen">
+                    <div class="texto">
+                        <p class="ejemplo">EJEMPLOS</p>
+                    </div>
+                </div>
+            </section>
+            <div class="menu2">
+                <ul class="items2" type="none">
+                    <li><a href="#" id="menugaleria" class="I">Inicio</a></li>
+                    <li><a href="#" id="menugaleria" class="E">Ejemplos</a></li>
+                    <li><a href="#" id="menugaleria" class="GF">Galeria Fotos</a></li>
+                    <li><a href="#" id="menugaleria" class="GE">Galeria Ejemplos 1</a></li>
+                </ul>
+            </div>
+            <div class="galeriaejemplo1">
+                <h1 id="ge1">Galeria Ejemplo 1</h1>
+                <h2 id="ge1">GALERÍAS</h2>
+                <h2 id="ge1">Galería Fotografías Ejemplo 1</h2>
+            </div>  
+            <div class="contenedorimagenes"></div>
+            <div class="contenedorpantalla"></div>
+            <div class="contenedorinfo">
+                <p class="info">" Esta Galeria es solo un Ejemplo, podemos hacer muchos mas tipos de Galerias "</p>
+                <p class="consulta">!!! Consultenos !!!</p>
+            </div>
+            <div class="imagen2">
+                <div class="texto2">
+                    <div class="oferta">
+                        <P class="ofertas">Recomendado ver <br> OFERTAS</p>
+                        <P class="super">!Super Precios!</P>
+                        <a href="#" class="boton1">VER OFERTAS - AQUI</a>
+                    </div>
+                    <div class="llama">
+                        <p class="llamanos">!!! Llamanos !!!</p>
+                        <p class="llamanos">Solo WhatsApp</p>
+                        <div class="boton2"><p class="fa-brands fa-whatsapp"></p>
+                            <p class="tel">669 645 117</p>
+                        </div>
+                        <a class="boton3">IR A CONTACTO-AQUI</a>
+                    </div>
+                </div>
+            </div>
+            <footer class="infocontacto">
+                <div class="barracontacto">
+                    <a href="" class="contacto">Inicio</a>
+                    <a href="" class="contacto">Apps</a>
+                    <a href="" class="contacto">Servicios Web</a>
+                    <a href="" class="contacto">Trabajos</a>
+                    <a href="" class="contacto">Ofertas</a>
+                    <a href="" class="contacto">Web Demo</a>
+                    <a href="" class="contacto">Blog</a>
+                    <a href="" class="contacto">Contacto</a>
+                </div>
+                <hr>
+                <div class="barracontactoinferior">
+                
+                <a href="#" class="logo"><img src="logo_inferior.webp" alt=""></a>
+                    <a href="" class="contactoinfeiror">Mapa Web</a>
+                    <a href="" class="contactoinfeiror">bazawebapp@gmail.com</a>
+                    <a href="" class="contactoinfeiror">Facebook</a>
+                </div>
+            </footer>      
+        </div>  
+    </div>
+`;
+let menu = document.querySelector('.menu');
+let btnmenu = document.querySelector('.btnmenu');
+let header = document.querySelector('header' );
+let btncerrar = document.querySelector('.btncerrar')
+let posicionmenu = window.pageYOffset;
+btnmenu.onclick = function (){
+    header.classList.toggle('mostrar');
+}
+btncerrar.onclick = function (){
+    header.classList.toggle('mostrar');
+}
+window.addEventListener('scroll', function(){
+    let posicionactual = window.pageYOffset;
+    if (posicionmenu >= posicionactual) {
+        header.style.top = "0px";
+
+    }else {
+        header.style.top = "-90px"; 
+    }
+    posicionmenu = posicionactual;
+})
+let minimo = 102;
+let maximo = 204;
+let arregloNumAle = [];
+let arreglo = [1,2,3,3];
+let topeAle;
+let numAle;
+function filtrar (){
+    arregloNumAle = arregloNumAle.filter((el,index)=>{return arregloNumAle.indexOf(el) === index;})
+}
+function completaNumeros() {
+    filtrar();
+    if (arregloNumAle.length < 20){ 
+        let restantes = (20 - arregloNumAle.length);
+        // console.log(restantes);
+        for (i = 0; i < restantes; i++) {
+            arregloNumAle.push(Math.floor(Math.random() * (1 + maximo - minimo) + minimo));      
+        }
+    }
+}
+function generaNumeros(){
+    for (i = 0;  i < 20; i++) {
+        let numeroGenerado;
+        // console.log(i);
+        numeroGenerado = Math.floor(Math.random() * (1 + maximo - minimo) + minimo);
+        // console.log(numeroGenerado);
+        // console.log(arregloNumAle);
+        arregloNumAle.push(numeroGenerado);
+        // arregloNumAle +=  numeroGenerado;
+    }
+    completaNumeros();
+    return arregloNumAle;
+}
+generaNumeros();
+const contenedorimagenes = document.querySelector('.contenedorimagenes');
+function generaGaleria( id, imagen) {
+    const galeria = `<a href="${id}" class="imagenesgaleria">
+    <img src=${imagen.download_url} alt="Imagen" class="imagenes" >
+    </a>`
+    contenedorimagenes.innerHTML += galeria;
+}
+for ( i = 0; i < arregloNumAle.length; i++) {
+    let img = "#img"+i;
+    if ((listaDeImagenes.find(elemento => {return elemento.id === arregloNumAle[i]})) != undefined) {
+        let id = listaDeImagenes.findIndex(elemento => {return elemento.id === arregloNumAle[i]});
+        generaGaleria(img, listaDeImagenes[id]);
+    }
+}
+const contenedorpantalla = document.querySelector('.contenedorpantalla');
+    function prueba(id, imagenes, atras, siguiente){
+        const pantalla = `<article class="pantallagaleria" id="${id}">
+        <a href="${atras}" class="atras" ><ion-icon name="arrow-back-outline"></ion-icon></a>
+        <img src=${imagenes.download_url} alt="Imagen">
+        <a href="${siguiente}" class="siguiente"><ion-icon name="arrow-forward-outline"></ion-icon></a>
+        <a href="" class="btnclose">X</a>
+        </article>`
+    contenedorpantalla.innerHTML += pantalla;
+}
+
+let atras = 0;
+let adelante = 20;
+for ( i = 0; i < 20; i++) {
+    adelante = adelante - 1;
+    atras  ++;
+    let img = "img"+i;
+    if ((listaDeImagenes.find(elemento => {return elemento.id === arregloNumAle[i]})) != undefined) {
+        let id = listaDeImagenes.findIndex(elemento => {return elemento.id === arregloNumAle[i]});
+        if( i == 0){
+            let idatras = "#img"+19;          
+            let idadelante = "#img"+1;          
+            prueba(img, listaDeImagenes[id], idatras, idadelante, idadelante);
+        }else if(i == 19){
+            let idatras = "#img"+18;          
+            let idadelante = "#img"+0;          
+            prueba(img, listaDeImagenes[id], idatras, idadelante, idadelante);
+        }else if( i >= 1 && i <= 19){
+            let idatras = "#img"+(atras-2);
+            let idadelante = "#img"+(atras);
+            prueba(img, listaDeImagenes[id], idatras, idadelante, idadelante, );
+        }
+    }
+}
